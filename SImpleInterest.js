9db57e96c6239fcb82b79d15fb5044add3eeff02
Sandeep Mopare <96640528+SandeepMopare@users.noreply.github.com>
2022-02-21 14:01:57 +0530

    changes made in Si

--- SImpleInterest.js
+++ SImpleInterest.js
@@ -20,6 +20,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, () => {
-  console.log("Server Started on port 3000");
+server.listen(8082, () => {
+  console.log("Server Started on port 8082");
 });
